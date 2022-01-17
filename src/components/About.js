9fe4f0f5cb771c929f41957facd21ff959a671b3
Ionut Ianchis <i.ionutz50@gmail.com>
2022-01-17 20:19:25 +0200

    About us page completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import '../styles/About.css'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 
 const About = () => {
+
+    useEffect(() => {
+        Aos.init({ duration: 3000 })
+    }, [])
     return (
         <div className='content-container about-container' id='about'>
+            
+            <div className='about-us-text-container'>
+                
+                <h2 className='section-header'>ABOUT US</h2>
+                
+				<p>
+					The occupational traffic permit is one of the most important
+					things in the company when carrying out freight transport.
+					In fact, it is a prerequisite for doing business traffic at
+					all.
+                </p>
+                
+                <div className='question-card'>
+                    <div className='card-bar'></div>
+                    <p>How do you do when you need to obtain a commercial traffic permit for freight transport to your business ?</p>
+                </div>
 
-        </div>
-    )
+            </div>
+            
+            <img data-aos='fade-left' className='about-illustration' src={require('../images/illus-about.svg').default} alt='' />
+		</div>
+	)
 }
 
 export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,17 +6,22 @@ const Home = () => {
     return (
 		<div className='content-container home-container'>
 			<h1>Your awesome traffic permit consultant.</h1>
-			
-            <button type='button' className='orange-btn'>
+
+			<button type='button' className='orange-btn'>
 				GET STARTED{' '}
 				<span>
 					<img
 						src={require('../images/arrow-getstarted.svg').default}
-						alt='' />
+						alt=''
+					/>
 				</span>
-            </button>
-            
-            <img className="car-img" src={require('../images/car-home.svg').default} alt='' />
+			</button>
+
+			<img
+				className='car-img'
+				src={require('../images/car-home.svg').default}
+				alt=''
+			/>
 		</div>
 	)
 }
